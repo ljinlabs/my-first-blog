0a7e328e95eca8ae96228a5553630d77338b5024
J Lee <ljinlabs@gmail.com>
2020-06-07 21:10:42 +0900

    tried a new footer with the branch thing

--- ljinlabs/settings.py
+++ ljinlabs/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR),'static','/blog/static/']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
